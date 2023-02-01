3da09aef24e187b6bbae4b3cbb06fce0451d81ed
HaydenNease <hayden.nease@gmail.com>
2023-01-31 21:59:50 -0500

    changed index, user, and thought models

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      // TODO GETTER GOES HERE
+    },
+    username: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    reactions: [reactionSchema],    
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
+    },
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }],
+    thoughts: [thoughtSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
